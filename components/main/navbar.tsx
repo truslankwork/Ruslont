5e2d4d2269b9d53e1cc5d6092465d823fba61061
truslankwork <truslan.kwork@gmail.com>
2025-10-14 20:44:18 +0500

    Update navbar.tsx

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -28,7 +28,7 @@ export const Navbar = () => {
             className="cursor-pointer"
           />
           <span className="hidden sm:block text-white font-bold tracking-wide">
-           I'm Ruslan | TRuslan
+           I&apos;m Ruslan | TRuslan
           </span>
         </Link>
 
