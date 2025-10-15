90108500b9d73d2a83282b91d26d5dc5a59556fd
truslankwork <truslan.kwork@gmail.com>
2025-10-15 08:50:51 +0500

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -101,73 +101,36 @@ export const SOCIALS = [
   },
 ] as const;
 
-export const FRONTEND_SKILL = [
-  {
-    skill_name: "HTML",
-    image: "html.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "CSS",
-    image: "css.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "JavaScript",
-    image: "js.png",
-    width: 65,
-    height: 65,
-  },
-  {
-    skill_name: "Tailwind CSS",
-    image: "tailwind.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "React",
-    image: "react.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Next.js 14",
-    image: "next.png",
-    width: 80,
-    height: 80,
-  },
-] as const;
+type Skill = {
+  skill_name: string;
+  image: string;
+  width: number;
+  height: number;
+};
 
-export const BACKEND_SKILL = [
-  {
-    skill_name: "Node.js",
-    image: "node.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Express.js",
-    image: "express.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "MongoDB",
-    image: "mongodb.png",
-    width: 40,
-    height: 40,
-  },
-] as const;
+export const FRONTEND_SKILL: Skill[] = [
+  { skill_name: "HTML", image: "html.png", width: 80, height: 80 },
+  { skill_name: "CSS", image: "css.png", width: 80, height: 80 },
+  { skill_name: "JavaScript", image: "js.png", width: 65, height: 65 },
+  { skill_name: "Tailwind CSS", image: "tailwind.png", width: 80, height: 80 },
+  { skill_name: "React", image: "react.png", width: 80, height: 80 },
+  { skill_name: "Next.js 14", image: "next.png", width: 80, height: 80 },
+];
+
+export const BACKEND_SKILL: Skill[] = [
+  { skill_name: "Node.js", image: "node.png", width: 80, height: 80 },
+  { skill_name: "Express.js", image: "express.png", width: 80, height: 80 },
+  { skill_name: "MongoDB", image: "mongodb.png", width: 40, height: 40 },
+];
 
 // FULLSTACK_SKILL FRONTEND va BACKEND skilllarni birlashtiradi
-export const FULLSTACK_SKILL = [
+export const FULLSTACK_SKILL: Skill[] = [
   ...FRONTEND_SKILL,
   ...BACKEND_SKILL,
-] as const;
+];
 
-export const OTHER_SKILL = [] as const;
+export const OTHER_SKILL: Skill[] = [];
+
 
 export const PROJECTS = [
   {
